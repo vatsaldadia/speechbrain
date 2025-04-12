8e28f26c6412b2d26dd2450091d6d4e977cea1f7
Vatsal Dadia <101032677+vatsaldadia@users.noreply.github.com>
2025-04-12 14:14:48 -0400

    Update Xvector.py

--- speechbrain/lobes/models/Xvector.py
+++ speechbrain/lobes/models/Xvector.py
@@ -153,7 +153,7 @@ class Classifier(sb.nnet.containers.Sequential):
         super().__init__(input_shape=input_shape)
 
         self.append(activation(), layer_name="act")
-        self.append(sb.nnet.normalization.BatchNorm1d, layer_name="norm")
+        # self.append(sb.nnet.normalization.BatchNorm1d, layer_name="norm")
 
         if lin_blocks > 0:
             self.append(sb.nnet.containers.Sequential, layer_name="DNN")
